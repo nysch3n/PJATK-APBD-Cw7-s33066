cbd1f53e76f9ccc3665ff32dfb07fe3782158011
Jakub Rębkowski <s33066@pjwstk.edu.pl>
2026-05-14 14:07:20 +0200

    dodanie warstwy DTO

--- DTOs/PCRequestDto.cs
+++ DTOs/PCRequestDto.cs
@@ -1,6 +1,23 @@
-﻿namespace Cw7.DTOs;
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace Cw7.DTOs;
 
 public class PCRequestDto
 {
-    
+    [Required]
+    [MaxLength(50)]
+    public string Name { get; set; } = string.Empty;
+
+    [Required]
+    public double Weight { get; set; }
+
+    [Required]
+    public int Warranty { get; set; }
+
+    [Required]
+    public DateTime CreatedAt { get; set; }
+
+    [Required]
+    public int Stock { get; set; }
 }
--- DTOs/PCResponseDto.cs
+++ DTOs/PCResponseDto.cs
@@ -1,6 +1,13 @@
-﻿namespace Cw7.DTOs;
+﻿using System;
+
+namespace Cw7.DTOs;
 
 public class PCResponseDto
 {
-    
+    public int Id { get; set; }
+    public string Name { get; set; } = string.Empty;
+    public double Weight { get; set; }
+    public int Warranty { get; set; }
+    public DateTime CreatedAt { get; set; }
+    public int Stock { get; set; }
 }
